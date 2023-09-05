a867a02feefc9eb6a151e1a3a59f4289145d1bca
conorhaithcoat <83032417+conorhaithcoat@users.noreply.github.com>
2023-09-05 11:58:40 -0600

    Add files via upload

---
+++ Detective.java
@@ -0,0 +1,9 @@
+
+public class Detective {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PublicSafety.java
@@ -0,0 +1,9 @@
+
+public class PublicSafety {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
